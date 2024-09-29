77032929e3def3d6f00a1a5ca63d6abed69836c5
Arun Kishore E <110062113+arunkishore2001@users.noreply.github.com>
2024-09-29 22:07:49 +0530

    Completed

---
+++ send_email.php
@@ -0,0 +1,23 @@
+<pre>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = "e.arunkishore@gmail.com"; // Change this to your Gmail address
+    $subject = "New Contact Form Submission: $subject";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    // Send email
+    if (mail($to, $subject, $body)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message could not be sent. Please try again later.";
+    }
+} else {
+    echo "Error: Invalid request.";
+}
+?>
+</pre>
